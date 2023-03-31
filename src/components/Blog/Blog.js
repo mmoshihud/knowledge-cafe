0820ce242f5278ade6097bccd4971eaac24fec6c
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2023-03-31 16:41:21 +0600

    date string to object, then show all blog data

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,12 +1,30 @@
+import { useEffect, useState } from "react";
 import BookmarkedBlog from "../BookmarkedBLog/BookmarkedBlog";
 import Card from "../Card/Card";
 
 const Blog = () => {
+  const [blog, setBlog] = useState([]);
+
+  useEffect(() => {
+    fetch("/blog.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const convertDate = data.map((item) => ({
+          ...item,
+          date: new Date(item.date),
+          countDays: Math.floor(
+            (new Date() - new Date(item.date)) / (1000 * 60 * 60 * 24)
+          ),
+        }));
+        setBlog(convertDate);
+      });
+  }, []);
+
   return (
     <div>
       <hr />
       <div className="flex gap-4">
-        <Card></Card>
+        <Card blog={blog}></Card>
         <BookmarkedBlog></BookmarkedBlog>
       </div>
     </div>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,38 +1,35 @@
 import blogger from "../../images/blogger-1.jpg";
-const Card = () => {
-  const date = new Date("2023-03-30");
-  const today = new Date();
-  const dateDifferenceInMs = today - date;
-  const countDays = Math.floor(dateDifferenceInMs / (1000 * 60 * 60 * 24));
-
+const Card = (props) => {
   return (
     <div className="card w-7/12 bg-base-100 shadow-2xl p-4 mt-4">
-      <figure className="mt-4">
-        <img
-          className="rounded-2xl"
-          src="https://images.unsplash.com/photo-1555066931-4365d14bab8c"
-          alt="Shoes"
-        />
-      </figure>
-      <div className="card-body">
-        <div className="flex">
-          <img className="w-24 rounded-full mr-4" src={blogger} alt="blogger" />
-          <div>
-            <h1 className="text-xl font-bold">Mr. Raju</h1>
-            <p className="text-sm">
-              {date.toDateString()} ({countDays} day
-              {countDays > 1 ? <span>s</span> : ""} ago )
-            </p>
+      {props.blog.map((blogs) => (
+        <>
+          <figure className="mt-4">
+            <img className="rounded-2xl" src={blogs.image} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <div className="flex">
+              <img
+                className="w-24 rounded-full mr-4"
+                src={blogger}
+                alt="blogger"
+              />
+              <div>
+                <h1 className="text-xl font-bold">{blogs.name}</h1>
+                <p className="text-sm">
+                  {blogs.date.toDateString()} ({blogs.countDays} day
+                  {parseInt(blogs.countDays) > 1 ? <span>s</span> : ""} ago )
+                </p>
+              </div>
+              <p className="text-sm text-right">{blogs.time} min to read</p>
+            </div>
+            <h2 className="card-title">{blogs.title}</h2>
+            <p>{blogs.tags}</p>
+            <p className="text-primary underline">Mark as read</p>
           </div>
-          <p className="text-sm text-right">05 min to read</p>
-        </div>
-        <h2 className="card-title">
-          Knowledge Cafe How to get your first job as a self- taught programmer
-        </h2>
-        <p>#beginners #programming</p>
-        <p className="text-primary underline">Mark as read</p>
-      </div>
-      <hr />
+          <hr />
+        </>
+      ))}
     </div>
   );
 };
